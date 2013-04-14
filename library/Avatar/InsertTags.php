036c7cdeeb36810f788399f91274d926bef59f63
Tristan Lins <tristan.lins@bit3.de>
2013-04-14 10:40:19 +0200

    Format code.

--- library/Avatar/InsertTags.php
+++ library/Avatar/InsertTags.php
@@ -9,7 +9,7 @@
  * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
  */
 
-namespace Avatar; 
+namespace Avatar;
 
 /**
  * Class InsertTags
@@ -19,49 +19,72 @@ namespace Avatar;
  */
 class InsertTags extends \System
 {
-  /**
-    * replace Inserttag
-    * @param string
-    * @return string
-    */
+	/**
+	 * replace Inserttag
+	 *
+	 * @param string
+	 *
+	 * @return string
+	 */
 	public function replaceTags($strTag)
 	{
 		$arrTag = trimsplit('::', $strTag);
-    
-    if($arrTag[0] != 'avatar' ) return false;
 
-    //get_Settings
-    $arrDims = deserialize($GLOBALS['TL_CONFIG']['avatar_maxdims']);
+		if ($arrTag[0] != 'avatar') {
+			return false;
+		}
 
-    if (!$arrTag[1]) 
-    {
-      if (!FE_USER_LOGGED_IN)
-        return '<img src="' . TL_FILES_URL . \Image::get("system/modules/avatar/assets/male.png", $arrDims[0], $arrDims[1], $arrDims[2]) . '" width="' . $arrDims[0] . '" height="' . $arrDims[1] . '" alt="Avatar" class="avatar">';
+		//get_Settings
+		$arrDims = deserialize($GLOBALS['TL_CONFIG']['avatar_maxdims']);
 
-      $strAvatar = \FrontendUser::getInstance()->avatar;
-      $strAlt = \FrontendUser::getInstance()->firstname . " " . \FrontendUser::getInstance()->lastname;
-      
-      if ( $strAvatar == '' && \FrontendUser::getInstance()->gender != '')
-        return '<img src="' . TL_FILES_URL . \Image::get("system/modules/avatar/assets/" . \FrontendUser::getInstance()->gender . ".png", $arrDims[0], $arrDims[1], $arrDims[2]) . '" width="' . $arrDims[0] . '" height="' . $arrDims[1] . '" alt="Avatar" class="avatar">';       
-    } 
-    elseif (is_numeric($arrTag[1]))
-    {
-      $objUser = \Database::getInstance()->prepare("SELECT * FROM tl_member WHERE id=?")
-                                           ->execute($arrTag[1]);
-      $strAvatar = $objUser->avatar;
-      $strAlt = $objUser->firstname . " " . $objUser->lastname;                                     
-    } 
-    
-    $objFile = \FilesModel::findByPk($strAvatar);   
+		if (!$arrTag[1]) {
+			if (!FE_USER_LOGGED_IN) {
+				return '<img src="' . TL_FILES_URL . \Image::get(
+					"system/modules/avatar/assets/male.png",
+					$arrDims[0],
+					$arrDims[1],
+					$arrDims[2]
+				) . '" width="' . $arrDims[0] . '" height="' . $arrDims[1] . '" alt="Avatar" class="avatar">';
+			}
 
-    if ($objFile !== null)
-    {
-      return '<img src="' . TL_FILES_URL . \Image::get($objFile->path, $arrDims[0], $arrDims[1], $arrDims[2]) . '" width="' . $arrDims[0] . '" height="' . $arrDims[1] . '" title="' . $strAlt . '" alt="' . $strAlt . '" class="avatar">';
-    }
-    else
-    {
-      return '<img src="' . TL_FILES_URL . \Image::get("system/modules/avatar/assets/male.png", $arrDims[0], $arrDims[1], $arrDims[2]) . '" width="' . $arrDims[0] . '" height="' . $arrDims[1] . '" alt="Avatar" class="avatar">';
-    }
+			$strAvatar = \FrontendUser::getInstance()->avatar;
+			$strAlt    = \FrontendUser::getInstance()->firstname . " " . \FrontendUser::getInstance()->lastname;
+
+			if ($strAvatar == '' && \FrontendUser::getInstance()->gender != '') {
+				return '<img src="' . TL_FILES_URL . \Image::get(
+					"system/modules/avatar/assets/" . \FrontendUser::getInstance()->gender . ".png",
+					$arrDims[0],
+					$arrDims[1],
+					$arrDims[2]
+				) . '" width="' . $arrDims[0] . '" height="' . $arrDims[1] . '" alt="Avatar" class="avatar">';
+			}
+		}
+		elseif (is_numeric($arrTag[1])) {
+			$objUser = \Database::getInstance()
+				->prepare("SELECT * FROM tl_member WHERE id=?")
+				->execute($arrTag[1]);
+			$strAvatar = $objUser->avatar;
+			$strAlt = $objUser->firstname . " " . $objUser->lastname;
+		}
+
+		$objFile = \FilesModel::findByPk($strAvatar);
+
+		if ($objFile !== null) {
+			return '<img src="' . TL_FILES_URL . \Image::get(
+				$objFile->path,
+				$arrDims[0],
+				$arrDims[1],
+				$arrDims[2]
+			) . '" width="' . $arrDims[0] . '" height="' . $arrDims[1] . '" title="' . $strAlt . '" alt="' . $strAlt . '" class="avatar">';
+		}
+		else {
+			return '<img src="' . TL_FILES_URL . \Image::get(
+				"system/modules/avatar/assets/male.png",
+				$arrDims[0],
+				$arrDims[1],
+				$arrDims[2]
+			) . '" width="' . $arrDims[0] . '" height="' . $arrDims[1] . '" alt="Avatar" class="avatar">';
+		}
 	}
 }
 
